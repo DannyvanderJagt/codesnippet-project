ce2aa179133d705a690296415654bc34472e3b73
Danny van der Jagt <dannyvanderjagt@gmail.com>
2015-05-11 12:35:43 +0200

    Added search logica

--- src/controllers/search.php
+++ src/controllers/search.php
@@ -5,6 +5,52 @@ class Search extends Controller
 	private $templates = PAGES['search']['templates'];
 
 	public function load($params = []){
+		if(isset($_GET['q']) && isset($_GET['l'])){
+			$this->searchNormal($_GET['q'], $_GET['l']);
+		}else if(isset($_GET['q'])){
+			$this->searchByQuery($_GET['q']);
+		}else if(isset($_GET['l'])){
+			$this->searchByLanguage($_GET['l']);
+		}else{
+			// No search query.
+			echo 'no search query!';
+		}
+
 		$this->display($this->templates['default']);
+	}
+
+	private function searchByQuery($query){
+		$this->snippet = $this->model('snippet');
+
+		$count = $this->snippet
+			->where('title', 'LIKE', '%'.$query.'%')
+			->orWhere('description', 'LIKE', '%'.$query.'%')
+			->count();
+
+		echo $count;
+	}
+
+	private function searchByLanguage($lang){
+		$this->snippet = $this->model('snippet');
+
+		$count = $this->snippet->where('Lang','=', $lang)
+		->count();
+
+		echo $count;
+	}
+
+	public function searchNormal($search, $lang){
+		$this->snippet = $this->model('snippet');
+
+		$count = $this->snippet
+			->where('Lang', 'LIKE', 2)
+			->where(function($query) use ($search){
+				$query
+					->where('title', 'LIKE', '%'.$search.'%')
+					->orWhere('description', 'LIKE', '%'.$search.'%');
+			})
+			->count();
+
+		echo $count;
 	}
 }
